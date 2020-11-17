a0e865ec69677c99404ac1d2e30df115b16c559c
herbert <herbert@oppolzer.at>
2020-11-17 09:57:59 +0100

    unused-specflow-clauses

--- README.md
+++ README.md
@@ -15,3 +15,9 @@ See blog post [Retrieving SP and CU of installed SQL Server instances](https://d
 retrieves FileVersion and ProductVersion of all installed instances of `sqlservr.exe`, the main executable of MS SQL Server, *and* looks up the ProductVersion in  Microsoft's [SQL Server Builds](https://aka.ms/SQLServerbuilds) Excel sheet
 
 See blog post [Retrieving SP and CU of installed SQL Server instances](https://devio.wordpress.com/2020/10/01/retrieving-sp-and-cu-of-installed-sql-server-instances/).
+
+## unused-specflow-clauses.ps1
+
+analyzes SpecFlow methods marked with [Given], [When], and [Then] attributes whether there `Regex`es are referenced in `.feature` files.
+
+See blog post [Finding unused SpecFlow step implementations with PowerShell](https://devio.wordpress.com/2020/11/17/finding-unused-specflow-step-implementations-with-powershell/)
---
+++ unused-specflow-clauses.ps1
@@ -0,0 +1,91 @@
+﻿
+$basedir = "path\to\bin"
+$specdll = "specflow-tests.dll"
+$featuredir = "path\to\feature\root"
+
+$dlls = get-childitem -path "$basedir\*.dll"
+foreach($dll in $dlls) {
+    write-host $dll
+    $dummy = [System.Reflection.Assembly]::LoadFrom($dll)
+}
+
+try
+{
+    $asm = [System.Reflection.Assembly]::LoadFrom("$basedir\$specdll", $null, 0)
+}
+catch
+{
+    $_.Exception.Message
+}
+
+try
+{
+    $types = $asm.GetTypes()
+}
+catch
+{
+    #write-host $_.Exception.Message
+    write-host $_.Exception.InnerException.LoaderExceptions
+}
+
+# SpecFlow attributes
+$binding = [TechTalk.SpecFlow.BindingAttribute]
+$given = [TechTalk.SpecFlow.GivenAttribute]
+$when = [TechTalk.SpecFlow.WhenAttribute]
+$then = [TechTalk.SpecFlow.ThenAttribute]
+
+# collect [Binding] classes
+$bindingclasses = $types | Where-Object { $_.IsDefined($binding, $false) }
+
+# collect SpecFlow method attributes
+$attributes = @()
+
+foreach($bc in $bindingclasses)  {
+    foreach($m in $bc.GetMethods()) {
+
+        $a = $m.GetCustomAttributes($given, $false)
+        if ($a.Length -gt 0) {
+            "Given " + $a[0].Regex
+            $attributes += $a[0]
+        }
+
+        $a = $m.GetCustomAttributes($when, $false)
+        if ($a.Length -gt 0) {
+            "When " + $a[0].Regex
+            $attributes += $a[0]
+        }
+
+        $a = $m.GetCustomAttributes($then, $false)
+        if ($a.Length -gt 0) {
+            "Then " + $a[0].Regex
+            $attributes += $a[0]
+        }
+    }
+}
+
+# collect .feature files
+$featurefiles = get-childitem -recurse -path "$featuredir\*.feature"
+$featurefilecontents = @{}
+foreach($ff in $featurefiles) {
+    $featurefilecontents[$ff] = Get-Content $ff
+}
+
+$foundattributes = @()
+$notfoundattributes = @()
+
+:Outer foreach($a in $attributes) {
+    foreach($ff in $featurefiles) {
+
+        $found = $featurefilecontents[$ff] | select-string -Pattern $a.Regex -CaseSensitive
+        if ($found.Matches.Length -gt 0) {
+            #write-host "found $($a.Regex) in file $ff"
+            $foundattributes += $a
+            continue Outer
+        }
+    }
+
+    write-host "did not found reference for $($a.GetType().Name) $($a.Regex)"
+    $notfoundattributes += $a
+}
+
+$notfoundattributes | select-object Regex
